8e620a12cbdac3095442cb94633a1426db0a4d21
csibivili <csibi.vili@gmail.com>
2019-08-18 11:49:42 +0200

    ILoginState & handlers

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -3,20 +3,46 @@ import * as React from 'react';
 export interface ILoginProps {
 }
 
-export default class Login extends React.Component<ILoginProps> {
+export interface ILoginState {
+  username: string;
+  password: string;
+
+  [key: string]: ILoginState[keyof ILoginState];
+}
+
+export default class Login extends React.Component<ILoginProps, ILoginState> {
+
+  constructor(props: ILoginProps) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let newState = { ...this.state };
+    newState[event.currentTarget.name] = event.currentTarget.value
+    this.setState(newState);
+  }
+
+  login = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  }
+
   public render() {
     return (
       <div className="container min-vh-100">
         <div className="row min-vh-100 justify-content-center align-items-center">
-          <form className="col-lg-3 col-sm-4 col-8 mx-auto">
+          <form className="col-lg-3 col-sm-4 col-8 mx-auto" onSubmit={this.login}>
             <div className="form-group">
               <label className="w-100">Username
-                  <input type="text" className="form-control" />
+                  <input type="text" className="form-control" name="username" onChange={this.handleChange}/>
               </label>
             </div>
             <div className="form-group">
               <label className="w-100">Password
-                  <input type="password" className="form-control" />
+                  <input type="password" className="form-control" name="password" onChange={this.handleChange}/>
               </label>
             </div>
             <button type="submit" className="btn btn-primary">Sign in</button>
