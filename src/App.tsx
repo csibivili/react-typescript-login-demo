30440131f4ba16afaba1c041aaf403565649c5a5
csibivili <csibi.vili@gmail.com>
2019-08-22 20:21:54 +0200

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 import Login from './components/login/Login';
 
 const App: React.FC = () => {
   return (
-      <Login />
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/login" />} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </Router>
   );
 }
 
