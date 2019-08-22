c67cdc7b9b04fc78f5da9b02d2331ba407a0329f
csibivili <csibi.vili@gmail.com>
2019-08-22 20:44:01 +0200

    add logout to home component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,13 +1,37 @@
 import * as React from 'react';
+import { Redirect } from 'react-router-dom';
 
 export interface IHomeProps {
 }
 
-export default class Home extends React.Component<IHomeProps> {
+export interface IHomeState {
+  isLoggedOut: boolean;
+}
+
+export default class Home extends React.Component<IHomeProps, IHomeState> {
+  constructor(props: IHomeProps) {
+    super(props);
+    this.state = {
+      isLoggedOut: false
+    }
+  }
+
+  logout = () => {
+    let newState = { ...this.state };
+    newState.isLoggedOut = true;
+    this.setState(newState);
+  }
+
   public render() {
+    const { isLoggedOut } = this.state;
+
+    if (isLoggedOut) {
+      return <Redirect to='/' />
+    }
+
     return (
       <div>
-        Home
+        <button className="btn btn-secondary" onClick={this.logout}>Logout</button>
       </div>
     );
   }
